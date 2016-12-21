74f23d669c91c78a2d15380be1ad0ee8eba9bea5
Michael Praschl <michael.praschl@gmail.com>
2016-12-21 23:05:19 +0100

    refactored drawing

--- Ruler/RulerTicksDisplay.cs
+++ Ruler/RulerTicksDisplay.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -7,6 +8,14 @@ namespace MiP.Ruler
 {
     public class RulerTicksDisplay : Canvas
     {
+        public enum RulerPositionValues
+        {
+            Left,
+            Top,
+            Right,
+            Bottom
+        }
+
         public RulerTicksDisplay()
         {
             Loaded += LeftRuler_Loaded;
@@ -16,135 +25,88 @@ namespace MiP.Ruler
 
         private void LeftRuler_Loaded(object sender, RoutedEventArgs e)
         {
-            if (RulerPosition == RulerPositionValues.Left)
-                DrawRulerLeft();
-
-            if (RulerPosition == RulerPositionValues.Right)
-                DrawRulerRight();
-
             if (RulerPosition == RulerPositionValues.Top)
-                DrawRulerTop();
+                DrawRuler(SystemParameters.PrimaryScreenWidth, 0, SetTickPosition, SetTickTextPositionHorizontal);
 
             if (RulerPosition == RulerPositionValues.Bottom)
-                DrawRulerBottom();
+                DrawRuler(SystemParameters.PrimaryScreenWidth, Height, SetTickPosition, SetTickTextPositionHorizontal);
+
+            if (RulerPosition == RulerPositionValues.Left)
+                DrawRuler(SystemParameters.PrimaryScreenHeight, 0, SetTickPositionSwapped, SetTickTextPositionVertical);
+      
+            if (RulerPosition == RulerPositionValues.Right)
+                DrawRuler(SystemParameters.PrimaryScreenHeight, Width, SetTickPositionSwapped, SetTickTextPositionVertical);
         }
 
-        private void DrawRulerLeft()
+        private void DrawRuler(double pixelCount, double tickOffset, 
+            Action<Line, double, double, double, double> setTickPosition, 
+            Action<TextBlock, int> setTickTextPosition)
         {
-            for (var y = 0; y <= SystemParameters.PrimaryScreenHeight + 2; y += 2)
-            {
-                var length = 4;
-                if (y % 10 == 0)
-                    length = 8;
-                if (y % 50 == 0)
-                    length = 12;
+            var isLeftTop = RulerPosition == RulerPositionValues.Left || RulerPosition == RulerPositionValues.Top;
 
-                var gridline = new Line
+            for (var pos = 0; pos < pixelCount + 2; pos += 2)
+            {
+                var length = GetLength(pos);
+
+                var tick = new Line
                 {
                     Stroke = Brushes.Black,
-                    StrokeThickness = 1.0,
-                    X1 = 0,
-                    X2 = length,
-                    Y1 = y,
-                    Y2 = y
+                    StrokeThickness = 1.0
                 };
-                Children.Add(gridline);
 
-                if (y > 0 && y % 100 == 0)
+                setTickPosition(tick, pos, pos, tickOffset - length, tickOffset + length);
+                
+                if (isLeftTop && pos > 0 && pos%100 == 0)
                 {
-                    var markBlock = new TextBlock { Text = y.ToString("0") };
-                    Children.Add(markBlock);
-                    markBlock.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
-                    SetLeft(markBlock, 12);
-                    SetTop(markBlock, y - markBlock.DesiredSize.Height / 2);
+                    var tickText = new TextBlock {Text = pos.ToString("0")};
+                    Children.Add(tickText);
+                    tickText.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+
+                    setTickTextPosition(tickText, pos);
                 }
+                
+                Children.Add(tick);
             }
         }
 
-        private void DrawRulerRight()
+        private static void SetTickTextPositionVertical(TextBlock tickText, int pos)
         {
-            for (var y = 0; y <= SystemParameters.PrimaryScreenHeight + 2; y += 2)
-            {
-                var length = 4;
-                if (y % 10 == 0)
-                    length = 8;
-                if (y % 50 == 0)
-                    length = 12;
-
-                var gridline = new Line
-                {
-                    Stroke = Brushes.Black,
-                    StrokeThickness = 1.0,
-                    X1 = Width,
-                    X2 = Width - length,
-                    Y1 = y,
-                    Y2 = y
-                };
-                Children.Add(gridline);
-            }
-        }
-        
-        private void DrawRulerTop()
-        {
-            for (var x = 0; x <= SystemParameters.PrimaryScreenWidth + 2; x += 2)
-            {
-                var length = 4;
-                if (x % 10 == 0)
-                    length = 8;
-                if (x % 50 == 0)
-                    length = 12;
-
-                var gridline = new Line
-                {
-                    Stroke = Brushes.Black,
-                    StrokeThickness = 1.0,
-                    X1 = x,
-                    X2 = x,
-                    Y1 = 0,
-                    Y2 = length
-                };
-                Children.Add(gridline);
-
-                if (x > 0 && x % 100 == 0)
-                {
-                    var markBlock = new TextBlock { Text = x.ToString("0") };
-                    Children.Add(markBlock);
-                    markBlock.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
-                    SetTop(markBlock, 12);
-                    SetLeft(markBlock, x - markBlock.DesiredSize.Width / 2);
-                }
-            }
+            var top = pos - tickText.DesiredSize.Height/2;
+            SetLeft(tickText, 12);
+            SetTop(tickText, top);
         }
 
-        private void DrawRulerBottom()
+        private static void SetTickTextPositionHorizontal(TextBlock tickText, int pos)
         {
-            Children.Clear();
-
-            for (var x = 0; x < SystemParameters.PrimaryScreenWidth + 2; x += 2)
-            {
-                var length = 4;
-                if (x % 10 == 0)
-                    length = 8;
-                if (x % 50 == 0)
-                    length = 12;
-
-                var gridline = new Line
-                {
-                    Stroke = Brushes.Black,
-                    StrokeThickness = 1.0,
-                    X1 = x,
-                    X2 = x,
-                    Y1 = Height,
-                    Y2 = Height - length
-                };
-                Children.Add(gridline);
-            }
+            var left = pos - tickText.DesiredSize.Width/2;
+            SetTop(tickText, 12);
+            SetLeft(tickText, left);
         }
 
-        public enum RulerPositionValues
+        private static void SetTickPosition(Line tick, double x1, double x2, double y1, double y2)
         {
-            Left, Top, Right, Bottom
+            tick.X1 = x1;
+            tick.X2 = x2;
+            tick.Y1 = y1;
+            tick.Y2 = y2;
+        }
+
+        public void SetTickPositionSwapped(Line tick, double x1, double x2, double y1, double y2)
+        {
+            SetTickPosition(tick, y1, y2, x1, x2);
+        }
+
+        private static int GetLength(int pos)
+        {
+            var length = 4;
+
+            if (pos%10 == 0)
+                length = 8;
+
+            if (pos%50 == 0)
+                length = 12;
+
+            return length;
         }
     }
-
 }

bff8bc12fde97c67f4f34c68c41dc865bbf7a6cb
Michael Praschl <praschl@users.noreply.github.com>
2016-12-22 14:10:49 +0100

    Update TODO.cs

--- Ruler/TODO.cs
+++ Ruler/TODO.cs
@@ -2,15 +2,17 @@
 
 // Lock automatic Horizontal / Vertical switching on drag-resize.
 
+// refactor ruler line + text into a small class.
+
 // Keep lines when switching
 // draw measuring between multiple lines
   // draw a blue box between lines, with size inside (outside if too small)
   // maybe draw the box only when mouseover
 
-// Option draw percentages instead of pixel
+// Option draw percentages instead of pixel, or additional to pixel (below)
   // When resizing keep % distance between lines
 
-// Let user drag origin from (0,0) 
+// Let user drag origin from (0,0) requires drawing negative part of ruler
 
 // Settings über Context Menü bzw F4
 // - Transparenz
